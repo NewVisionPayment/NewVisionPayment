ff7f07b2d859d8ed4ce6c5f6e2272f4649d4cdcb
Tom <tommakesmusic@users.noreply.github.com>
2021-09-28 22:52:01 +0100

    NewVision Payment Databse / Website
    Initial commit. First working version.
    
    Needs functionality added and cosmetics worked on but that's for the team to join in on!
    
    Current Functionality only reads from the database.

---
+++ NewVisionPayment/Services/CosmosDbService.cs
@@ -0,0 +1,71 @@
+﻿namespace NewVisionPayment.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using NewVisionPayment.Models;
+    using Microsoft.Azure.Cosmos;
+    using Microsoft.Azure.Cosmos.Fluent;
+    using Microsoft.Extensions.Configuration;
+    using System;
+
+    public class CosmosDbService : ICosmosDbService
+    {
+        private Container _container;
+
+        public CosmosDbService(
+            CosmosClient dbClient,
+            string databaseName,
+            string containerName)
+        {
+            this._container = dbClient.GetContainer(databaseName, containerName);
+        }
+
+        public List<NewVisionCustomer> customers { get; set; } = new List<NewVisionCustomer>();
+
+        public event Action OnChange;
+
+        public async Task AddItemAsync(NewVisionCustomer customer)
+        {
+            await this._container.CreateItemAsync<NewVisionCustomer>(customer, new PartitionKey(customer.Id));
+        }
+
+        public async Task DeleteItemAsync(string id)
+        {
+            await this._container.DeleteItemAsync<NewVisionCustomer>(id, new PartitionKey(id));
+        }
+
+        public async Task<NewVisionCustomer> GetItemAsync(string id)
+        {
+            try
+            {
+                ItemResponse<NewVisionCustomer> response = await this._container.ReadItemAsync<NewVisionCustomer>(id, new PartitionKey(id));
+                return response.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+
+        }
+
+        public async Task<List<NewVisionCustomer>> GetItemsAsync(string queryString)
+        {
+            var query = this._container.GetItemQueryIterator<NewVisionCustomer>(new QueryDefinition(queryString));
+            List<NewVisionCustomer> results = new List<NewVisionCustomer>();
+            while (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+
+                results.AddRange(response.ToList());
+            }
+
+            return results;
+        }
+
+        public async Task UpdateItemAsync(string id, NewVisionCustomer customer)
+        {
+            await this._container.UpsertItemAsync<NewVisionCustomer>(customer, new PartitionKey(id));
+        }
+    }
+}
---
+++ NewVisionPayment/Services/ICosmosDbService.cs
@@ -0,0 +1,20 @@
+﻿namespace NewVisionPayment.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using NewVisionPayment.Models;
+
+    public interface ICosmosDbService
+    {
+        event Action OnChange;
+
+        List<NewVisionCustomer> customers { get; set; }
+
+        Task<List<NewVisionCustomer>> GetItemsAsync(string query);
+        Task<NewVisionCustomer> GetItemAsync(string id);
+        Task AddItemAsync(NewVisionCustomer customer);
+        Task UpdateItemAsync(string id, NewVisionCustomer customer);
+        Task DeleteItemAsync(string id);
+    }
+}
