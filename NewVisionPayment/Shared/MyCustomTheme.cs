e38002d7775abf352d6111b18ca742db4aa8d6ec
Tom <tommakesmusic@users.noreply.github.com>
2021-09-30 13:04:40 +0100

    A few more parts to add

---
+++ NewVisionPayment/Shared/MyCustomTheme.cs
@@ -0,0 +1,16 @@
+﻿<MudThemeProvider Theme="MyCustomTheme" />
+
+
+@code{
+
+    MudTheme MyCustomTheme = new MudTheme()
+    {
+
+        Palette = new Palette()
+        {
+
+        Primary = "#594ae2ff",
+        }
+    }
+
+}
