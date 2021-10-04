f666ff3edc729f136447cbbd979689f25ffc6300
Tom <tommakesmusic@users.noreply.github.com>
2021-10-04 09:42:59 +0100

    Update chooseCustomer.cs
    Adding comments.

--- NewVisionPayment/Classes/chooseCustomer.cs
+++ NewVisionPayment/Classes/chooseCustomer.cs
@@ -5,12 +5,12 @@ using NewVisionPayment.Models;
 using NewVisionPayment.Services;
 using NewVisionPayment.Controllers;
 
-
+// This is our customer object that will be passed as a parameter
 namespace NewVisionPayment.Classes
 {
     public class chooseCustomer
     {
-
+        // A customer object that unless given a full set of values will be created as a blank object
         public static NewVisionCustomer ChosenCustomer { get; set; } = new NewVisionCustomer();
 
     }
